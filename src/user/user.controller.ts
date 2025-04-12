783ce57f84510e28966dc7970369d3779411f75c
Muhammad Tarek <mohammadtarek000@gmail.com>
2025-04-12 17:06:34 +0200

    add edit user functionality

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { user } from 'generated/prisma';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { EditUserDto } from './dto/edit-user.dto';
 import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private service: UserService) {}
 
   @Get('me')
   getMe(@GetUser() user: user) {
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: string, @Body() dto: EditUserDto) {
+    return this.service.editUser(userId, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { DbService } from 'src/db/db.service';
+import { EditUserDto } from './dto/edit-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private db: DbService) {}
+
+  async editUser(userId: string, dto: EditUserDto) {
+    const user = await this.db.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new NotFoundException('User not Found');
+
+    return await this.db.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        first_name: dto.first_name ?? dto.first_name,
+        last_name: dto.last_name ?? dto.last_name,
+        email: dto.email ?? dto.email,
+      },
+      select: {
+        first_name: true,
+        last_name: true,
+        email: true,
+      },
+    });
+  }
 }
