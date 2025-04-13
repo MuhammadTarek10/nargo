39bf521802a8e540f92b5e6ecfa5bdd5f55ce9fe
Muhammad Tarek <mohammadtarek000@gmail.com>
2025-04-13 20:58:09 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './utils/filters';
 
@@ -11,6 +12,16 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new AllExceptionsFilter());
+  const config = new DocumentBuilder()
+    .setTitle('My NestJS API')
+    .setDescription('The API description for my application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
